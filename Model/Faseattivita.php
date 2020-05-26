4648208d2041ab62c337d0649c95f9afd1d1780d
Massimo Infunti <massimoi@nkoni.org>
2020-05-26 14:57:41 +0200

    correzione cache fasi

--- Model/Faseattivita.php
+++ Model/Faseattivita.php
@@ -129,11 +129,14 @@ class Faseattivita extends AppModel {
 	//returns a list wich is good for a combobox
 	public function getSimple($attivita_id = null, $solo_entrata = 0,$solo_aperte=0)
 	{
-/* 		if (Configure::read('debug')==0)
+ 		if (Configure::read('debug')==0)
         {
 			$fa = Cache::read("faseattivita_2level_{$attivita_id}_{$solo_entrata}_{$solo_aperte}", 'long');
-			return $fa;
-        } */
+			if (!empty($fa))
+			{
+				return $fa;
+			}
+        } 
 		
 		$this->recursive = -1;
 		$conditions = array();
@@ -166,7 +169,7 @@ class Faseattivita extends AppModel {
                             '{n}.Attivita.name'
                            );
 		$fa = Hash::merge($notset, $fa);
-		//Cache::write("faseattivita_2level_{$attivita_id}_{$solo_entrata}_{$solo_aperte}", $fa, 'long');
+		Cache::write("faseattivita_2level_{$attivita_id}_{$solo_entrata}_{$solo_aperte}", $fa, 'long');
         return $fa;
 	}
 	
